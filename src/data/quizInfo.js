1fb12063aa6fee7c4a0f9ec73a8aa9373862c6bb
formacion-cristiana <ofs.bariloche@gmail.com>
2026-09-16 19:05:40 +0200

    Update quizInfo.js

--- src/data/quizInfo.js
+++ src/data/quizInfo.js
@@ -1,5 +1,5 @@
 export const qIds = ["C01", "C02", "C03","C04","C05","C06","C07","C08","C09","C10"];//,"C11"]; // add more as needed
 
-export const qTitle = "Catecumenado San Francisco"
+export const qTitle = "Repaso del Evangelio según San Mateo"
 
-export const whatsappNumber = "5492994188966"
+export const whatsappNumber = "393500251975"//"5492994188966"
