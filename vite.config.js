5c991884aa5b16bfdd40740b69ff0dd6cf055eae
formacion-cristiana <ofs.bariloche@gmail.com>
2026-09-16 18:16:48 +0200

    Update vite.config.js
    corregi la base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/",
+  base: "/mateo/",
   plugins: [
     react(),
   ],
